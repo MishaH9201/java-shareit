ddd348936afb9387be78fcf89513aa64c2b09fe2
MishaH9201 <herlos9201@gmail.com>
2022-08-21 18:58:21 +0300

    sprint 13

--- src/main/java/ru/practicum/shareit/requests/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/requests/ItemRequest.java
@@ -3,9 +3,6 @@ package ru.practicum.shareit.requests;
 import lombok.Data;
 import ru.practicum.shareit.user.model.User;
 
-
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.FutureOrPresent;
 import java.time.LocalDateTime;
 @Data
 public class ItemRequest {
